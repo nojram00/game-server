6bb03ea1b65371e4b7488be79449284379acbb93
Nojram00 <petmalu.marjon@gmail.com>
2024-09-13 12:36:12 +0800

    Update route.ts

--- src/app/api/get-students/route.ts
+++ src/app/api/get-students/route.ts
@@ -1,20 +1,22 @@
 import StudentModel from "@main/models/student";
-import { Student } from "@main/types/types";
+import { db, Student } from "@main/models_v2/drizzle";
+// import { Student } from "@main/types/types";
 import { NextRequest, NextResponse } from "next/server";
 
 
 export async function GET(req : NextRequest) {
     try
     {
-        const students = await StudentModel.getAll()
+        // const students = await StudentModel.getAll()
+        const students = db.select().from(Student)
 
         if (students){
 
-            if (students.length <= 0){
-                return NextResponse.json({
-                    messsage: "No students found..."
-                }, {status : 400})
-            }
+            // if (students.length <= 0){
+            //     return NextResponse.json({
+            //         messsage: "No students found..."
+            //     }, {status : 400})
+            // }
 
             return NextResponse.json({
                 data : students
