223027895861baedfe07efd124a376d8bc9c6849
Nojram00 <petmalu.marjon@gmail.com>
2024-09-23 10:37:29 +0800

    Update route.ts

--- src/app/api/update-score/[studentId]/route.ts
+++ src/app/api/update-score/[studentId]/route.ts
@@ -1,4 +1,7 @@
-import { updateStudentScore } from "@main/models_v2/drizzle"
+import { db, updateStudentScore } from "@main/models_v2/drizzle"
+import { Score, Student } from "@main/models_v2/schema"
+import { eq } from "drizzle-orm"
+
 import { NextRequest, NextResponse } from "next/server"
 
 interface Params {
@@ -9,10 +12,39 @@ export async function POST(req : NextRequest, {params} : { params : Params }) {
 
     const { postTest, preTest } = await req.json()
 
-    const data = await updateStudentScore(studentId, {
-        postTest : postTest,
-        preTest : preTest
-    })
+    let data;
+
+    const student = await db.query.Student.findFirst({
+        where : eq(Student.id, Number(studentId))
+    });
+
+    // update postest only:
+    if(preTest === null || preTest === undefined){
+        const d = await db.update(Score)
+                            .set({ postTest : Number(postTest) })
+                            .where(eq(Score.id, Number(student?.score)))
+                            .returning();
+
+        data = d[0];
+    }
+    // update pretest only:
+    else if (postTest === null || postTest === undefined)
+    {
+        const d = await db.update(Score)
+                            .set({ preTest : Number(preTest) })
+                            .where(eq(Score.id, Number(student?.score)))
+                            .returning();
+        data = d[0];
+    }
+    // update all
+    else
+    {
+        data = await updateStudentScore(studentId, {
+            postTest : postTest,
+            preTest : preTest
+        })
+    }
+
 
     if (data){
 
